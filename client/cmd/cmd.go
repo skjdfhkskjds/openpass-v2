32f9e2101bce94be894b0cf651483cfd268eb3da
skjdfhkskjds <colin.chuu@gmail.com>
2024-03-13 21:40:18 -0400

    pp

--- client/cmd/cmd.go
+++
@@ -1,5 +0,0 @@
-package cmd
-
-// start client command
-// this will be a running routine that just spins and allows you to submit requests to the server its listening to
-
--- server/server.go
+++
@@ -1,4 +0,0 @@
-package server
-
-// start server command
-// this will start the server and expose a port for the client to connect to and send requests blah blah
